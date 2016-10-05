365394ddb4f9442a470e62ee0778f6454a2a9453
Robert Coleman <rcoleman@fool.com>
2016-10-05 09:42:53 -0400

    Update image_resizer.py

--- image_resizer.py
+++ image_resizer.py
@@ -1,19 +1,21 @@
 from PIL import Image
-from scipy.misc import imresize
+from resizeimage import resizeimage
 import os
 import sys
 
+
 def resize(folder, file_name):
-    file_path = os.path.join(folder, file_name)
-    im = Image.open(file_path)
-    new_im = imresize(im, size=(150, 150))  # replace with desired dims
-    new_im.save(file_path + "copy.jpg")
+	file_path = os.path.join(folder, file_name)
+	im = Image.open(file_path)
+	new_im = resizeimage.resize_width(im, <width>)    # insert desired width here, or change to `resize_height`
+	new_im.save(file_path + "copy.jpg")
+
 
 def bulkResize(folder):
-    imgExts = ["jpg"]
-    for path, dirs, files in os.walk(folder):
-        for file_name in files:
-            resize(path, file_name)
+	imgExts = ["jpg"]
+	for path, dirs, files in os.walk(folder):
+		for file_name in files:
+			resize(path, file_name)
 
 
 if __name__ == "__main__":
